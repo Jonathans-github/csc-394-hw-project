de7320b27133e1d33fa16b06569a907d868b4172
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-04-21 23:01:24 -0500

    This the cahnges
    I made the cjanges

--- homework/homework/urls.py
+++ homework/homework/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("student", include('students.urls')),
+    path('admin/', admin.site.urls)]
